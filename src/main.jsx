8b76880177b77df0f8d3069f17ee1acca61c7b4a
Esteban Lagos <81320905+Estebanlv06@users.noreply.github.com>
2023-11-25 12:21:45 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './Pages/Home/Home'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './Pages/Home/Home';
+import './index.css';
 import 'primeicons/primeicons.css';
         
 
